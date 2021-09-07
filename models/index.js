5af4b5eda0e32aabc6ce0633aa340046290ded55
Bomb Atomically <ieugenelee@gmail.com>
2021-09-07 15:22:07 -0700

    fixed comment.js

--- models/index.js
+++ models/index.js
@@ -40,9 +40,9 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+// Comment.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
 
 Comment.belongsTo(Post, {
     foreignKey: 'post_id'
